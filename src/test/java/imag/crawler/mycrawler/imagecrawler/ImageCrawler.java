7a136121c53f684c6cc2157813845151b5f999da
imag2046 <imag2046nust@163.com>
2015-04-21 21:39:52 +0800

    refine

--- src/test/java/imag/crawler/mycrawler/imagecrawler/ImageCrawler.java
+++ src/test/java/imag/crawler/mycrawler/imagecrawler/ImageCrawler.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package imag.crawler.mycrawler.imagecrawler;
 
 import imag.crawler.crawler.Page;
